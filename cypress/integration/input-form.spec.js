5ab5b78a473f08e1710e9502a6feb4292e4bed62
seanmcquaid <sean.mcquaid2@verizon.net>
2019-04-05 10:42:55 -0400

    added on change and visit testing for cypress

--- cypress/integration/input-form.spec.js
+++ cypress/integration/input-form.spec.js
@@ -1,5 +1,16 @@
 describe("Input form", ()=>{
+    beforeEach(()=>{
+        cy.visit("/")
+    })
     it("focuses input on load", ()=>{
-        cy.visit("localhost:3030")
+        cy.focused()
+        .should("have.class", "new-todo")
+    })
+    it.only("accepts input", ()=>{
+        const typedText = "Buy Milk"
+        
+        cy.get(".new-todo")
+            .type(typedText)
+            .should("have.value", typedText)
     })
 })
