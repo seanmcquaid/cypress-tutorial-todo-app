1a17600e39cd60b1c71f622f029a32287f9c4853
seanmcquaid <sean.mcquaid2@verizon.net>
2019-04-06 20:56:31 -0400

    implemented some more spec and will finish this later

--- cypress/integration/input-form.spec.js
+++ cypress/integration/input-form.spec.js
@@ -11,6 +11,13 @@ describe("Input form", ()=>{
         
         cy.get(".new-todo")
             .type(typedText)
-            .should("have.value", typedText)
+            .should("have.value", typedText);
+    })
+    context("Form submission", ()=>{
+        it.only("Adds a new todo on submit", ()=>{
+            cy.get(".new-todo")
+                .type("Buy eggs")
+                .type("{enter}")
+        })
     })
 })
