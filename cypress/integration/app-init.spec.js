4da3a3ada934ad59d380332089f35ce28a573ec1
seanmcquaid <sean.mcquaid2@verizon.net>
2019-04-08 12:01:31 -0400

    added in axios api call for todo testing

--- cypress/integration/app-init.spec.js
+++ cypress/integration/app-init.spec.js
@@ -24,6 +24,10 @@ const todos = [
 describe("App initialization", ()=>{
     it.only("Loads todos on page load", ()=>{
         cy.server();
-        cy.route("GET", "/api/todos", );
+        cy.route("GET", "/api/todos", todos);
+        cy.visit("/")
+
+        cy.get(".todo-list li")
+            .should("have.length", 4)
     })
 })
--- src/lib/service.js
+++ src/lib/service.js
@@ -1,3 +1,6 @@
 import axios from "axios";
 
 export const saveTodo = (todo) => axios.post("http://localhost:3030/api/todos", todo);
+
+export const loadTodos = () =>
+    axios.get("http://localhost:3030/api/todos");
