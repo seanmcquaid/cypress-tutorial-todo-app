54f9d681a3c393da0f363720ffa28f5b06a78127
seanmcquaid <sean.mcquaid2@verizon.net>
2019-04-09 19:12:18 -0400

    wrote tests for the footer activity

--- cypress/integration/footer.spec.js
+++ cypress/integration/footer.spec.js
@@ -1,9 +1,35 @@
+import { isSymbol } from "util";
+
 describe("Footer", ()=>{
     context("with a single todo", ()=>{
-        it.only("displays a singular todo in count", ()=>{
+        it("displays a singular todo in count", ()=>{
             cy.seedAndVisit([{id: 1, name: "Buy milk", isComplete: false}])
             cy.get(".todo-count")
                 .should("contain", "1 todo left")
         })
+    })
+    context("with multiple todos", ()=>{
+        beforeEach(()=>{
+            cy.seedAndVisit()
+        })
+        it("displays plural todos in count", ()=>{
+
+            cy.get(".todo-count")
+                .should("contain", "3 todos left")
+        })
+        it.only("Handles filter links", ()=>{
+            const filters = [
+                {link: "Active", expectedLength: 3},
+                {link: "Completed", expectedLength: 1},
+            ]
+            cy.wrap(filters).each(filter => {
+                cy.contains(filter.link)
+                .click()
+            
+                cy.get(".todo-list li")
+                    .should("have.length", filter.expectedLength)
+            })
+        })
+        
     })
 })
--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -4,6 +4,7 @@ import TodoForm from './TodoForm'
 import TodoList from './TodoList'
 import Footer from './Footer'
 import {saveTodo, loadTodos, destroyTodo, updateTodo} from "../lib/service"
+import {filterTodos} from "../lib/utils"
 
 
 export default class TodoApp extends Component {
@@ -87,10 +88,11 @@ export default class TodoApp extends Component {
             />
           </header>
           <section className="main">
-            <TodoList todos={this.state.todos} 
-            handleDelete={this.handleDelete}
-            handleToggle ={this.handleToggle}
-            />
+          <Route path="/:filter?" render={({match}) => 
+          <TodoList todos={filterTodos(match.params.filter, this.state.todos)} 
+          handleDelete={this.handleDelete}
+          handleToggle ={this.handleToggle}/>
+          }/>
           </section>
           <Footer remaining={remaining}/>
         </div>
