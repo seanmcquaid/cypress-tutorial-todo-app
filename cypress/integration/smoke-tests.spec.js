14f71a773615900565868ff94e5f7dc0910d661f
seanmcquaid <sean.mcquaid2@verizon.net>
2019-04-09 20:03:46 -0400

    learned more about cy.wait being used to test behavior that may delay our front end tests from passing

--- cypress/integration/smoke-tests.spec.js
+++ cypress/integration/smoke-tests.spec.js
@@ -28,4 +28,49 @@ describe("Smoke tests",()=>{
                 })
         })
     })
+
+    context("With active todos", ()=>{
+        beforeEach(()=>{
+            cy.fixture("todos")
+                .each(todo =>{
+                    const newTodo = Cypress._.merge(todo, {isComplete: false})
+                    cy.request("POST", "/api/todos", newTodo)
+                })
+            cy.visit("/")
+        })
+        it("Loads existing data from the DB", ()=>{
+            cy.get(".todo-list li")
+                .should("have.length", 4)
+        })
+        it("Deletes todos", () => {
+            cy.server()
+            cy.route("DELETE", "/api/todos/*")
+                .as("delete")
+            cy.get(".todo-list li")
+                .each($el => {
+                    cy.wrap($el)
+                        .find(".destroy")
+                        .invoke("show")
+                        .click()
+                    cy.wait("@delete")
+                })
+                .should("not.exist")
+        })
+        it.only("Toggles todos", ()=>{
+            cy.server()
+            cy.route("PUT", "/api/todos/*")
+                .as("update")
+            cy.get(".todo-list li")
+                .each($el =>{
+                    cy.wrap($el)
+                        .as("item")
+                        .find(".toggle")
+                        .click()
+
+                    cy.wait("@update")
+                    cy.get("@item")
+                        .should("have.class", "completed")
+                })
+        })
+    })
 })
